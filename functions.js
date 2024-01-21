b8bfd3aa3d10af7a6075eb828f61dd0d795826aa
Anna <ambrooks.271@gmail.com>
2024-01-21 15:43:18 -0600

    pomodoro timer, still some issues

--- functions.js
+++ functions.js
@@ -138,3 +138,113 @@ function updateTodoUI() {
 window.onload = function() {
   retrieveFromLocalStorage();
 };
+
+
+// POMODORO FUNCTIONS
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  longBreakInterval: 4
+};
+
+let interval;
+
+const mainButton = document.getElementById('start');
+mainButton.addEventListener('click', () => {
+  const { action } = mainButton.dataset;
+  if (action === 'start') {
+    startTimer();
+  } else {
+    startTimer();
+  }
+});
+
+const modeButtons = document.querySelector('#js-mode-buttons');
+modeButtons.addEventListener('click', handleMode);
+
+function handleMode(event) {
+  const { mode } = event.target.dataset;
+  if (!mode) return;
+  
+  switchMode(mode);
+  stopTimer();
+}
+
+function switchMode(mode) {
+  timer.mode = mode;
+  timer.remainingTime = {
+    total: timer[mode] * 60,
+    minutes: timer[mode],
+    seconds: 0,
+  };
+
+  document.querySelectorAll('button[data-mode]')
+    .forEach(e => e.classList.remove('active'));
+  document.querySelector(`[data-mode="${mode}"]`).classList.add('active');
+  updateClock();
+}
+
+function updateClock() {
+  const { remainingTime } = timer;
+  const minutes = `${remainingTime.minutes}`.padStart(2, '0');
+  const seconds = `${remainingTime.seconds}`.padStart(2, '0');
+
+  const min = document.getElementById('minutes');
+  const sec = document.getElementById('seconds');
+
+  console.log(min);
+  console.log(sec)
+
+  min.textContent = minutes;
+  sec.textContent = seconds;
+}
+
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  mainButton.dataset.action = 'stop';
+  mainButton.textContent = 'stop';
+  mainButton.classList.add('active');
+
+  interval = setInterval(function() {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+function getRemainingTime(endTime) {
+  const currentRTime = Date.parse(new Date());
+  const difference = endTime - currentRTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds
+  };
+};
+
+function clearInterval(interval) {
+  interval = 0;
+}
+
+function stopTimer() {
+  clearInterval(interval);
+  mainButton.dataset.action = 'start';
+  mainButton.textContent = 'start';
+  mainButton.classList.remove('active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  switchMode('pomodoro');
+})
